f04000e4e6f6c0b915ef75376f0a3cb9a0e67163
jimmymani79 <167142865+jimmymani79@users.noreply.github.com>
2024-05-06 11:05:59 -0400

    Update VetTests.java
    fixed

--- src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetTests.java
@@ -28,7 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class VetTests {
 
     @Test
-    public void testSerialization()
+    public void testSerialization() {
         Vet vet = new Vet();
         vet.setFirstName("Zaphod");
         vet.setLastName("Beeblebrox");
